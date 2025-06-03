7adcb87210de1136c233f396d95ef6b1717ba4a2
Anurag-Kumar-Poddar <anuragkumarpoddar736@gmail.com>
2025-06-03 08:19:53 +0530

    database added

---
+++ src/main/java/com/anurag/sprintgboot_github_actions/Employee.java
@@ -0,0 +1,47 @@
+package com.anurag.sprintgboot_github_actions;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	int age;
+	String name;
+	String gender;
+	String country;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
--- src/main/java/com/anurag/sprintgboot_github_actions/GitController.java
+++ src/main/java/com/anurag/sprintgboot_github_actions/GitController.java
@@ -1,12 +1,31 @@
 package com.anurag.sprintgboot_github_actions;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
+	@Autowired
+	EmployeeRepository erepo;
 	@RequestMapping("/test")
 	public String test() {
 		return "Test successfull.";
+	}
+	
+	@PostMapping("/save") 
+	public String save(@RequestBody Employee employee) {
+		erepo.save(employee);
+		return "data saved successfully";
+	}
+	
+	@GetMapping("/all")
+	public List<Employee> all() {
+		return erepo.findAll();
 	}
 }
